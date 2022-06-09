7c6ccc5c0ecc5895b2d5255694d238ad6e2b55e5
danzemt <nedix.py@gmail.com>
2022-06-09 16:47:05 +0300

    TEST 1.5 Hour done by 1 hour and 15 minutes 5/7

--- 001_study/TEST 1.5 HOUR.py
+++ 001_study/TEST 1.5 HOUR.py
@@ -1,13 +1,14 @@
+#N N1 #################
 # name = input(f'Please write your name:')
 # lastname = input(f'Please write your lastname: ')
 # age = input(f'Please write your age: ')
 #
 # print(f'Hello, {lastname} {name}. Your age is: {age}')
 
-
+# N2 ##################
 # c2=a2+b2
 # c = sqrt(a+a + b+b)
-
+#
 # a = input(f'Write lenght of catet a:')
 # b = input(f'Write lenght of catet b:')
 #
@@ -16,7 +17,7 @@
 # print(c)
 
 
-
+### N3 ######################
 # a, b, c = float(input('Side A:')), float(input('Side B:')), float(input('Side C:'))
 #
 # if c ** 2 == a ** 2 + b ** 2:
@@ -24,7 +25,7 @@
 # else:
 #     print(f'Triangle is not straight!')
 
-
+### N4 ################################
 # sample_list = [3, 2, 1, 6]
 # sample_list2 = ['Daniil', 'Danik', 'Denis']
 #
@@ -41,7 +42,7 @@
 # new_list.reverse()
 # print(new_list)
 
-
+### N5 ##########################
 # kortez = (1, 2, 3, 4, 5, 6)
 # kortez2= ('Kot', 'Dog', 'Bog')
 # kortez = list(kortez)
@@ -50,5 +51,25 @@
 # print(kortez)
 
 
+# N6
 lis = [1, 2, 3, 4, 7, 9, 9]
-for num in lis
+lis = list(lis)
+num = ()
+print(lis.count(9))
+
+if lis.count(1) >=2:
+    lis = '1'
+elif lis.count(2) >= 2:
+    lis = '2'
+elif lis.count(3) >= 2:
+    lis = '3'
+elif lis.count(4) >= 2:
+    lis = '4'
+elif lis.count(7) >= 2:
+    lis = '5'
+elif lis.count(9) >= 2:
+    lis = '9'
+
+print(lis)
+
+
